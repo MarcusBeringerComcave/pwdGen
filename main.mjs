89f38a5eb7cdd1feebe799426a310c07745b906b
marcellopagano <marpag72@virgilio.it>
2022-12-19 14:19:30 +0100

    update

--- main.mjs
+++ main.mjs
@@ -1,33 +1,50 @@
-import { pwdGen } from "./src/pwdgen.mjs"
+import { pwdGenWeak, pwdGenStrong, pwdCheck } from "./src/pwdgen.mjs"
 
+const pwdCheckbox = document.querySelectorAll('input[type="checkbox"]')
 const pwdMessage = document.getElementById('pwd-message')
 const pwdCopy = document.getElementById('pwd-copy')
 const pwdLabel = document.getElementById('pwd-label')
 const pwdInput = document.getElementById('pwd-input')
 const pwdRange = document.getElementById('pwd-range')
-const pwdGenerate = document.getElementById('pwd-generate')
+const pwdGenerateWeak = document.getElementById('pwd-generate-weak')
+const pwdGenerateStrong = document.getElementById('pwd-generate-strong')
 
 const pwdLabelView = () => pwdLabel.innerHTML = `<i class="fa-solid fa-key"></i> : ${pwdRange.value}`
 
-pwdCopy.addEventListener('click', function() {
+const pwdLabelCheck = () => {
+    Object.values(pwdCheck(pwdInput.value)).forEach((v, idx) => {
+        return v == true ? pwdCheckbox[idx].checked = true : pwdCheckbox[idx].checked = false
+    })
+}
+
+pwdCopy.addEventListener('click', function () {
+    console.log('123');
     try {
-        navigator.clipboard.writeText(pwdInput.value).then(()=>{
+        navigator.clipboard.writeText(pwdInput.value).then(() => {
             pwdMessage.classList.add('pwd-message-ani')
             setTimeout(() => pwdMessage.classList.remove('pwd-message-ani'), 1500)
         })
     } catch (err) {
-        pwdMessage.textContent = err 
+        pwdMessage.textContent = err
         pwdMessage.classList.add('pwd-message-ani')
-    }   
+    }
 })
 
 pwdRange.addEventListener('input', () => {
     pwdLabelView()
 })
 
-pwdGenerate.addEventListener('click', () => {
-    pwdInput.value = pwdGen(pwdRange.value)
+pwdGenerateWeak.addEventListener('click', () => {
+    pwdInput.value = pwdGenWeak(pwdRange.value)
+    pwdLabelCheck()
+})
+
+pwdGenerateStrong.addEventListener('click', () => {
+    pwdInput.value = pwdGenStrong(pwdRange.value)
+    pwdLabelCheck()
 })
 
 pwdLabelView()
-pwdInput.value = pwdGen()
+pwdLabelCheck()
+// start with default value maxRange=12
+pwdInput.value = pwdGenWeak()
--- src/pwdgen.mjs
+++ src/pwdgen.mjs
@@ -1,7 +1,21 @@
 const numbers = []
 const lettersLower = []
 const lettersUpper = []
-const signs = ['@', '#', '*', '^', '!', '$', '%', '&', '+', '(', ')']
+const signs = ['@', '#', '*', '^', '!', '?', '$', '%', '&', '+', '-', '=', '(', ')']
+
+// @params pwd : string 
+export const pwdCheck = (pwd) => {
+    const checkNumbers = pwd.split('').some(v => numbers.includes(v))
+    const checkLetterLower = pwd.split('').some(v => lettersLower.includes(v))
+    const checkLetterUpper = pwd.split('').some(v => lettersUpper.includes(v))
+    const checkSigns = pwd.split('').some(v => signs.includes(v))
+    return {
+        numbers: checkNumbers,
+        letterLower: checkLetterLower,
+        lettersUpper: checkLetterUpper,
+        signs: checkSigns
+    }
+}
 
 // numbers 0-9
 for (let i = 48; i <= 57; i++) {
@@ -15,9 +29,28 @@ for (let i = 65; i <= 90; i++) {
 for (let i = 65; i <= 90; i++) {
     lettersUpper.push(String.fromCharCode(i).toUpperCase())
 }
-export const pwdGen = (max = 12) => {
+// @params max : integer 
+export const pwdGenWeak = (max = 12) => {
     return [...numbers, ...lettersLower, ...lettersUpper, ...signs]
         .sort(() => Math.random() - 0.5)
         .slice(0, parseInt(max))
         .join('')
+}
+// @params max : integer 
+export const pwdGenStrong = (max = 12) => {
+    const arr = []
+    let tmp = []
+    let count = 0
+
+    for (let i = 0; i < max; i++) {
+        numbers.sort(() => Math.random() - 0.5)
+        lettersLower.sort(() => Math.random() - 0.5)
+        lettersUpper.sort(() => Math.random() - 0.5)
+        signs.sort(() => Math.random() - 0.5)
+        tmp = [numbers[0], lettersLower[0], lettersUpper[0], signs[0]]
+        arr.push(tmp[count])
+        count++
+        if (count > 3) count = 0
+    }
+    return arr.join('')
 }
